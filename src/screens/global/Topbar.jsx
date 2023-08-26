4e2b3505c568fcb37c3386856f8935bd830da9bc
Prashansha <manraldivyanshi46@gmail.com>
2023-08-26 23:18:36 +0530

    Theme toggler with Topbar is added

---
+++ src/screens/global/Topbar.jsx
@@ -0,0 +1,43 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme.js";
+import { InputBase } from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box display="flex" bgcolor={colors.primary[400]} borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchOutlinedIcon />
+        </IconButton>
+      </Box>
+      {/* Icons */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          <LightModeIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+        <IconButton>
+          <Person2OutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
